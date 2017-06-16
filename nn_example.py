77c651abe85a7e9ee63b0cbe5ca95e2e4abc973f
jserowik <jserowik@protonmail.com>
2017-06-16 16:39:24 -0400

    Added updated code and examples

--- nn_example.py
+++ nn_example.py
@@ -52,23 +52,28 @@ class NeuralNetwork():
 if __name__ == "__main__":
 
     #Intialise a single neuron neural network.
-    neural_network = NeuralNetwork()
+	neural_network = NeuralNetwork()
 
-    print "Random starting synaptic weights: "
-    print neural_network.synaptic_weights
+	print "Random starting synaptic weights: "
+	print neural_network.synaptic_weights
 
     # The training set. We have 4 examples, each consisting of 3 input values
-    # and 1 output value.
-    training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
-    training_set_outputs = array([[0, 1, 1, 0]]).T
+    # and 1 output value. Here we have a pattern of (A AND B) for [A, B, C|
+	training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1], [1, 0, 0]])
+	training_set_outputs = array([[0, 1, 0, 0, 0]]).T
 
     # Train the neural network using a training set.
     # Do it 10,000 times and make small adjustments each time.
-    neural_network.train(training_set_inputs, training_set_outputs, 10000)
+	neural_network.train(training_set_inputs, training_set_outputs, 10000)
 
-    print "New synaptic weights after training: "
-    print neural_network.synaptic_weights
+	print "New synaptic weights after training: "
+	print neural_network.synaptic_weights
+    
+	#Our test values
+	test_array = [1, 1, 0]
 
-    # Test the neural network with a new situation.
-    print "Considering new situation [1, 0, 0] -> ?: "
-    print neural_network.think(array([1, 0, 0]))
+	# Test the neural network with a new situation. Should be clode to 1
+	print "Considering new situation: "
+	print test_array
+	print "Result: "
+	print neural_network.think(array(test_array))
